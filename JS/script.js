c45a76a3f1c483dcd426da244d2b9fc941c14b5a
Ibrahim Abdelnaby <info.ibrahimabdelnaby@gmail.com>
2024-05-04 14:50:29 +0300

    devfolio

---
+++ JS/script.js
@@ -0,0 +1,10 @@
+
+var typed = new Typed('#element', {
+    strings: ['Designer', 'Developer', 'Freelancer'],
+    typeSpeed: 100,
+    loop: true,
+    loopCount: Infinity,
+    fadeOut: true,
+    fadeOutClass: 'typed-fade-out',
+    fadeOutDelay: 500
+});
